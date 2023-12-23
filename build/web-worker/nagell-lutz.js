da82e884e842d22d6b483510e0d2e15b6a5b0b78
anthroplankton <anthroplankton@users.noreply.github.com>
2023-12-23 06:15:28 +0000

    deploy: 622e711a706dbaeff90ff85bb8d26abf3f858c8a

--- build/web-worker/nagell-lutz.js
+++ build/web-worker/nagell-lutz.js
@@ -1,2 +1,2 @@
-!function(){"use strict";self.onmessage=n=>{const s=n.data;for(const n of function*(n){for(let s=1n,e=1n;s<=n;s+=e+=2n)n%s==0n&&(yield e+1n>>1n)}(s))postMessage(n);postMessage(null)}}();
+!function(){"use strict";onmessage=n=>{const s=n.data;for(const n of function*(n){for(let s=1n,o=1n;s<=n;s+=o+=2n)n%s==0n&&(yield o+1n>>1n)}(s))postMessage(n);postMessage(null)}}();
 //# sourceMappingURL=nagell-lutz.js.map
